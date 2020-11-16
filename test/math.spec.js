2def32319b7f9a6b02aa42ab2aa383616f899caa
rodrigo33 <rbarbosa.barbosa@gmail.com>
2020-11-16 11:40:50 -0300

    utilizando hook, para garantir a execução do valor antes

--- test/math.spec.js
+++ test/math.spec.js
@@ -6,20 +6,28 @@ const Math = require('../src/math')
 //descrevendo o meu teste
 //primeiro argumento descrição do que estou testando 
 //segundo arg. função, que os testes serão escritos
-describe('Math class', function(){
+let value = 0
+describe('Math class', function(){    
     //comportamento esperado pela minha classe
+    //hooks - formas de executar código e evitar repetição 
+    //beforeEach e beforeAfter, garante que antes ou após a execução será modificado o valor
+    beforeEach(function() {
+        value = 0
+    })
     it('Sum two numbers', function(done){
         const math = new Math();
         this.timeout(3000)
+
+        value = 5
         
-        math.sum(5, 5, value => {
+        math.sum(value, 5, value => {
             assert.equal(value, 10)
             done()
         })        
     })
-    it.skip('Multiply two numbers', function () {
+    it('Multiply two numbers', function () {
         const math = new Math();
         //função para testar e validar o comportamento
-        assert.equal(math.multiply(5,5), 25)
+        assert.equal(math.multiply(value, 5), 0)
     })
 })
